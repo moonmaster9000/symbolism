b6ca71a649c3393b2989f3112f6841d51d9ea341
Matt Parker <moonmaster9000@gmail.com>
2011-03-12 11:28:40 -0500

    require the activesupport string core ext

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,4 +1,2 @@
 $LOAD_PATH.unshift './lib'
-
-require 'active_support/core_ext/string'
 require 'symbolism'
--- lib/symbolism.rb
+++ lib/symbolism.rb
@@ -1,2 +1,3 @@
+require 'active_support/core_ext/string'
 require 'symbolism/class'
 require 'symbolism/symbol'
